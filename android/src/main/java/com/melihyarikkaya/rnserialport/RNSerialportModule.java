acdd70b16d3e521f93ccf98353957c47f926885c
aktripathi <aktripathi@nvidia.com>
2023-05-29 16:32:04 +0530

    Updated flag to PendingIntent.FLAG_IMMUTABLE

--- android/src/main/java/com/melihyarikkaya/rnserialport/RNSerialportModule.java
+++ android/src/main/java/com/melihyarikkaya/rnserialport/RNSerialportModule.java
@@ -560,7 +560,7 @@ public class RNSerialportModule extends ReactContextBaseJavaModule {
   private void requestUserPermission() {
     if(device == null)
       return;
-    PendingIntent mPendingIntent = PendingIntent.getBroadcast(reactContext, 0 , new Intent(ACTION_USB_PERMISSION), 0);
+    PendingIntent mPendingIntent = PendingIntent.getBroadcast(reactContext, 0 , new Intent(ACTION_USB_PERMISSION), PendingIntent.FLAG_IMMUTABLE);
     usbManager.requestPermission(device, mPendingIntent);
   }
 
